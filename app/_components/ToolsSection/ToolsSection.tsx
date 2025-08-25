f1aaffee41ccc2ad319060f8304b3d9b7576fdf3
Ricardo Sousa <rikisousa.9@gmail.com>
2025-08-25 17:55:16 +0100

    chore: send stashed updates

--- app/_components/ToolsSection/ToolsSection.tsx
+++ app/_components/ToolsSection/ToolsSection.tsx
@@ -5,6 +5,8 @@ import { Section } from "@/components/layout";
 import ToolsByTypeSegment from "./ToolsByTypeSegment";
 import OtherToolsSegment from "./OtherToolsSegment";
 import ToolsByEndSegment from "./ToolsByEndSegment";
+import GroupToggle from "./GroupToggle";
+import BgGraphics from "./BgGraphics";
 
 export default function ToolsSection() {
   const [isDefaultToggle, setIsDefaultToggle] = useState(true);
@@ -13,30 +15,16 @@ export default function ToolsSection() {
     <Section className={styles.toolsSection}>
       <div className={styles.headerFrame}>
         <h2 className={styles.toolsText}>My main Tools</h2>
-        <div className={styles.toggleFrame}>
-          <button
-            className={
-              !isDefaultToggle ? styles.toggleButtonActive : styles.toggleButton
-            }
-            onClick={() => setIsDefaultToggle(false)}
-          >
-            By End
-          </button>
-          <span className={styles.toggleDivisor}>{`/`}</span>
-          <button
-            className={
-              isDefaultToggle ? styles.toggleButtonActive : styles.toggleButton
-            }
-            onClick={() => setIsDefaultToggle(true)}
-          >
-            By Type
-          </button>
-        </div>
+        <GroupToggle
+          isToggled={isDefaultToggle}
+          setState={setIsDefaultToggle}
+        />
       </div>
       <div className={styles.toolsFrame}>
         {!isDefaultToggle ? <ToolsByEndSegment /> : <ToolsByTypeSegment />}
         <OtherToolsSegment />
       </div>
+      <BgGraphics />
     </Section>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,16 @@
 import { Page } from "@/components/layout";
 import { ContactSection } from "@/app/_components";
 import { HeroSection } from "@/app/_components/HeroSection";
-import { JobHistorySection } from "@/app/_components/JobHistorySection";
 import { LatestWorkSection } from "@/app/_components/LatestWorkSection";
 import { ToolsSection } from "./_components/ToolsSection";
+import { LookingForJobSection } from "./_components/LookingForJobSection";
 
 export default function HomePage() {
   return (
     <Page pageSrHeader="My Work">
       <HeroSection />
-      <JobHistorySection />
+      <LookingForJobSection />
       <LatestWorkSection />
-      <video width="30%" autoPlay muted loop playsInline>
-        <source src="/assets/videos/logo-animation.webm" type="video/webm" />
-        Your browser does not support the video tag.
-      </video>
       <ToolsSection />
       <ContactSection />
     </Page>
