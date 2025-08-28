74969329705d063e5c8cd817c16158a31cb21828
Ricardo Sousa <rikisousa.9@gmail.com>
2025-08-28 19:44:50 +0100

    fix: fix unused vars build error

--- components/feedback/DotLoader/DotLoader.tsx
+++ components/feedback/DotLoader/DotLoader.tsx
@@ -1,11 +1,6 @@
 import styles from "./DotLoader.module.css";
 import { HTMLAttributes } from "react";
 
-type DotLoaderProps = {
-  fill?: string;
-  size?: number;
-};
-
 export default function DotLoader({
   ...props
 }: HTMLAttributes<HTMLDivElement>) {
